20e73ecec3a7f33875eea0369aeecba797695c97
Viktor Bachmanov <vbachmanov@mail.ru>
2025-11-01 16:25:40 +0300

    excplicitly set provider IPX

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/image', '@nuxt/ui'],
   css: ['~/assets/css/main.css'],
+
+  image: {
+    provider: 'ipx',
+  }
 })
